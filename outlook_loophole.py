3ac7a72df09c5cac6dd19b421b8f45f4ff2cd39a
vivekvarma12 <33152570+vivekvarma12@users.noreply.github.com>
2021-03-18 18:11:41 +0530

    Add files via upload

---
+++ outlook_loophole.py
@@ -0,0 +1,37 @@
+import smtplib
+import re
+from email.mime.text import MIMEText
+from email.header    import Header
+from datetime import date
+userid = 'unknown@cisco.com'
+passid = "anypassword"
+today = str(date.today())
+#below is used to import file information
+'''status = open('Todays_Status.txt','r')
+str = status.read()
+i = re.search(today,str).end()
+for j in range(i,len(str)):
+	if str[j] == ';':
+		break
+today_status = str[i:j]'''
+today_status = "\nhello how r u\n"
+Salutation = 'Hi,\n'
+greeting = '\nRegards,\nDevil.'
+today_status = Salutation + today_status + greeting
+receivers = ['vidoddip@cisco.com']
+message = MIMEText(today_status,'plain','utf-8')
+message['subject'] = Header('Chapter Close','utf-8')
+message['from'] = userid
+message['to'] = ', '.join(receivers)
+smtpObj = smtplib.SMTP('mail.cisco.com')
+#smtpObj.set_debuglevel(1)
+try:
+	print("Sending.....")
+	smtpObj.starttls()
+	smtpObj.sendmail(userid, receivers, message.as_string())
+	print("Successfully sent email")
+except Exception as e:
+	print(e)
+	print("Error: unable to send email")
+finally:
+	smtpObj.quit()
